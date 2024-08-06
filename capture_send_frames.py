e428d1cd6b90318f605afe6303e586ad24f56ce4
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-06 16:00:07 -0300

    CHore: test

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -32,7 +32,7 @@ kinesis_client = boto3.client('kinesis',
                               aws_access_key_id=aws_access_key,
                               aws_secret_access_key=aws_secret_key)
 
-logger.info(f"Logging: {kinesis_client}")
+logger.info(f"Logging: {kinesis_client.text}")
 
 def send_frame_to_kinesis(frame_data):
     try:
