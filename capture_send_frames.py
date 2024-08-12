341b7147f8fbab9695f575bf5f7cea9fc73c089a
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-12 12:42:28 -0300

    Chore: new test pipeline

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -51,8 +51,6 @@ def capture_frames():
         '!', 'decodebin',
         '!', 'videorate', 'drop-only=true', 'max-rate=1',  # Processar apenas 1 frame por segundo
         '!', 'queue', 'leaky=downstream', 'max-size-buffers=1',
-        '!', 'videoscale',  # Adiciona escalonamento de vídeo para ajuste de resolução
-        '!', 'video/x-raw,width=640,height=360',  # Define a resolução para 640x360
         '!', 'kvssink', f'stream-name={kvs_stream_name}', f'aws-region={aws_region}', f'access-key={aws_access_key}',
         f'secret-key={aws_secret_key}'
     ]
