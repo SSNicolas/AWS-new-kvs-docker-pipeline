cab56590511d83979f482ebc09f9ef210044587f
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-21 09:35:40 -0300

    Chore: testing new pipeline

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -45,7 +45,7 @@ def on_eos(bus, msg):
 def capture_frames():
     pipeline_str = (
         f"rtspsrc location={camera_url} latency=0 ! "
-        "rtph264depay ! h264parse ! queue leaky=downstream ! "
+        "rtpmp4vdepay ! mpeg4videoparse ! queue leaky=downstream ! "
         f"kvssink stream-name={kvs_stream_name} storage-size=512 "
         f"aws-region={aws_region} access-key={aws_access_key} secret-key={aws_secret_key}"
     )
