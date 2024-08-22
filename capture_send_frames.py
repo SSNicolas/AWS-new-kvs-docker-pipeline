378c6d01b5c1adc11201796aaca184fd453b452f
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-22 10:50:35 -0300

    Chore: testing new pipeline

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -1,19 +1,18 @@
 import os
-import time
 import logging
 import gi
-
 
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 
-
+# Carregar variáveis de ambiente
 camera_url = os.getenv('RTSP_URL')
 kvs_stream_name = os.getenv('KVS_STREAM_NAME')
 aws_region = os.getenv('AWS_REGION')
 aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
+# Configurar logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -22,44 +21,56 @@ logger.info(f"KVS_STREAM_NAME: {kvs_stream_name}")
 logger.info(f"AWS_REGION: {aws_region}")
 logger.info(f"AWS_ACCESS_KEY_ID: {aws_access_key}")
 
-if not aws_region:
-    raise ValueError("AWS_REGION environment variable is not set.")
+# Verificar se todas as variáveis de ambiente estão configuradas
+if not all([camera_url, kvs_stream_name, aws_region, aws_access_key, aws_secret_key]):
+    raise ValueError("One or more environment variables are not set.")
 
-logger.info(f"Client created.")
+logger.info("Client created.")
 
 # Inicializando o GStreamer
 Gst.init(None)
 
 
-def on_error(bus, msg):
-    logger.error(f"Error: {msg.parse_error()}")
+def on_error(bus, msg, loop):
+    err, debug = msg.parse_error()
+    logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
+    logger.error(f"Debugging information: {debug if debug else 'None'}")
+    loop.quit()
 
 
-def on_eos(bus, msg):
+def on_eos(bus, msg, loop):
     logger.info("End-Of-Stream reached.")
-    Gst.main_quit()
+    loop.quit()
 
 
 def capture_frames():
     pipeline_str = (
-        f"rtspsrc location={camera_url} latency=0 ! "
-        "rtph264depay ! h264parse ! queue leaky=downstream ! "
+        f"rtspsrc location={camera_url} latency=200 ! "
+        "rtph264depay ! h264parse ! "
         f"kvssink stream-name={kvs_stream_name} storage-size=512 "
         f"aws-region={aws_region} access-key={aws_access_key} secret-key={aws_secret_key}"
     )
 
     pipeline = Gst.parse_launch(pipeline_str)
+    if not pipeline:
+        logger.error("Failed to create pipeline")
+        return
+
+    loop = GObject.MainLoop()
 
     bus = pipeline.get_bus()
     bus.add_signal_watch()
-    bus.connect("message::error", on_error)
-    bus.connect("message::eos", on_eos)
+    bus.connect("message::error", on_error, loop)
+    bus.connect("message::eos", on_eos, loop)
 
     logger.info("Starting the GStreamer pipeline.")
-    pipeline.set_state(Gst.State.PLAYING)
+    ret = pipeline.set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        logger.error("Unable to set the pipeline to the playing state.")
+        pipeline.set_state(Gst.State.NULL)
+        return
 
     try:
-        loop = GObject.MainLoop()
         loop.run()
     except Exception as e:
         logger.error(f"Exception in GStreamer loop: {e}")
