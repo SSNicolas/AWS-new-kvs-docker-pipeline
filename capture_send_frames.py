0b401bd7a1264181ac247ad5618967d6e8c261ec
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-22 16:10:11 -0300

    Chore: testing new pipeline

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -1,7 +1,9 @@
 import os
-import boto3
 import subprocess
 import logging
+import dotenv
+
+dotenv.load_dotenv('/app/.env')
 
 camera_url = os.getenv('RTSP_URL')
 kvs_stream_name = os.getenv('KVS_STREAM_NAME')
@@ -20,15 +22,10 @@ logger.info(f"AWS_ACCESS_KEY_ID: {aws_access_key}")
 if not aws_region:
     raise ValueError("AWS_REGION environment variable is not set.")
 
-kvs_client = boto3.client('kinesisvideo',
-                          region_name=aws_region,
-                          aws_access_key_id=aws_access_key,
-                          aws_secret_access_key=aws_secret_key)
-
 logger.info(f"Client created.")
 
 # Substituir a pipeline anterior pelo comando fornecido
-command = [ # Adiciona debug para o GStreamer
+command = [
     'gst-launch-1.0',
     'rtspsrc', f'location={camera_url}', 'short-header=TRUE',
     '!', 'rtph264depay',
@@ -40,11 +37,24 @@ command = [ # Adiciona debug para o GStreamer
 try:
     logger.info("Starting GStreamer pipeline.")
     # Iniciar o pipeline GStreamer
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     # Monitorar o processo continuamente
     while True:
         output = process.stdout.readline()
+        error = process.stderr.readline()
+
+        if output:
+            logger.info(f"STDOUT: {output.strip()}")
+
+        if error:
+            logger.error(f"STDERR: {error.strip()}")
+
+        # Se o processo terminar, sair do loop
+        if process.poll() is not None:
+            break
+
+    process.wait()
 
 except Exception as e:
     logger.error(f"Erro ao executar o pipeline GStreamer: {e}")
