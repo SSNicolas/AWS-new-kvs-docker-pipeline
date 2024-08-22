2cc101b925052b3709659edc58f597fe8b8ac951
SSNicolas <nicolas.gerbi@smartstaff.com.br>
2024-08-22 16:47:11 -0300

    Chore: testing new pipeline

--- capture_send_frames.py
+++ capture_send_frames.py
@@ -1,4 +1,5 @@
 import os
+import time
 import subprocess
 import logging
 import dotenv
@@ -18,47 +19,33 @@ logger.info(f"RTSP_URL: {camera_url}")
 logger.info(f"KVS_STREAM_NAME: {kvs_stream_name}")
 logger.info(f"AWS_REGION: {aws_region}")
 logger.info(f"AWS_ACCESS_KEY_ID: {aws_access_key}")
+logger.info(f"AWS_SECRET_ACCESS_KEY: {aws_secret_key}")
 
 if not aws_region:
     raise ValueError("AWS_REGION environment variable is not set.")
 
 logger.info(f"Client created.")
 
-# Substituir a pipeline anterior pelo comando fornecido
-command = [
-    'gst-launch-1.0',
-    'rtspsrc', f'location={camera_url}', 'short-header=TRUE',
-    '!', 'rtph264depay',
-    '!', 'video/x-h264,format=avc,alignment=au',
-    '!', 'kvssink', f'stream-name={kvs_stream_name}', 'storage-size=512',
-    f'access-key={aws_access_key}', f'secret-key={aws_secret_key}', f'aws-region={aws_region}'
-]
-
-try:
-    logger.info("Starting GStreamer pipeline.")
-    # Iniciar o pipeline GStreamer
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    # Monitorar o processo continuamente
+def capture_frames():
     while True:
-        output = process.stdout.readline()
-        error = process.stderr.readline()
+        command = [
+            'gst-launch-1.0',
+            'rtspsrc', f'location={camera_url}',
+            '!', 'rtph264depay',
+            '!', 'h264parse',
+            '!', 'kvssink', f'stream-name={kvs_stream_name}', 'storage-size=512', f'aws-region={aws_region}', f'access-key={aws_access_key}', f'secret-key={aws_secret_key}'
+        ]
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            logger.info(f"Process command.")
+            time.sleep(2)
+        except subprocess.CalledProcessError as e:
+            print(f"Erro ao executar o pipeline GStreamer: {e}")
+            print(e.stderr.decode())  # Imprime o erro retornado pelo GStreamer
 
-        if output:
-            logger.info(f"STDOUT: {output.strip()}")
 
-        if error:
-            logger.error(f"STDERR: {error.strip()}")
 
-        # Se o processo terminar, sair do loop
-        if process.poll() is not None:
-            break
 
-    process.wait()
-
-except Exception as e:
-    logger.error(f"Erro ao executar o pipeline GStreamer: {e}")
-finally:
-    if process:
-        process.terminate()
-        logger.info("GStreamer pipeline terminated.")
+if __name__ == "__main__":
+    logger.info("Starting frame capture and send to Kinesis")
+    capture_frames()
